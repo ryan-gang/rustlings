99c3e72ab3c4be9475b63ee42bcbabe4821eb75f
Ryan Gang <ryan-gg@outlook.com>
2025-02-27 17:37:07 +0530

    feat: finish upto enums

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -1,4 +1,4 @@
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
     vec.push(88);
@@ -20,7 +20,7 @@ mod tests {
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
 
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(&vec0);
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -8,9 +8,13 @@
 // - However, if Mary buys more than 40 apples, the price of each apple in the
 // entire order is reduced to only 1 rustbuck!
 
-// TODO: Write a function that calculates the price of an order of apples given
-// the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    let mut price = 2;
+    if quantity > 40 {
+        price = 1;
+    }
+    price * quantity
+}
 
 fn main() {
     // You can optionally experiment here.
